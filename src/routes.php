74e422487feb91cc227bff008ec13925018d3170
robert.lane <rob@lanosmedia.co.uk>
2019-02-06 19:57:33 +0000

    CORS Failsafe added

--- src/routes.php
+++ src/routes.php
@@ -27,3 +27,20 @@ $app->group('/api', function(){
 	// YOU CAN ALSO PROTECT A WHOLE GROUP, OR EVEN THE WHOLE APP!
 
 });
+
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+	return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+	$response = $next($req, $res);
+	return $response
+		->withHeader('Access-Control-Allow-Origin', 'http://mysite')
+		->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+		->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+	$handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+	return $handler($req, $res);
+});
